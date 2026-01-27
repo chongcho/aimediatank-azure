8549989422a91e7f258acb2eefc985da93d7aad9
James Cho <james.cho@aimediatank.com>
2026-01-27 15:17:30 -0800

    Remove top padding - media starts directly below navbar

--- src/app/media/[mediaId]/MediaPageClient.tsx
+++ src/app/media/[mediaId]/MediaPageClient.tsx
@@ -251,8 +251,8 @@ export default function MediaPageClient({ mediaId }: { mediaId: string }) {
 
   return (
     <div className="pb-[500px]">
-      {/* Media Player - Full Width */}
-      <div className="w-full bg-black pt-5">
+      {/* Media Player - Full Width, starts at top */}
+      <div className="w-full bg-black">
         <MediaPlayer
           type={media.type}
           url={media.url}
